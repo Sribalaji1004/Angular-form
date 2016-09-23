f08d69208f58e6a030c044ff1897a36034444e2e
Sribalaji1004 <sribalaji1004@outlook.com>
2016-09-23 18:37:56 +0530

    leaving office

--- app/Car.js
+++ app/Car.js
@@ -1,16 +1,9 @@
 "use strict";
 var Car = (function () {
-<<<<<<< HEAD
     function Car(id, name, power, alterEgo) {
         this.id = id;
         this.name = name;
         this.power = power;
-=======
-    function Car(id, name, Models, alterEgo) {
-        this.id = id;
-        this.name = name;
-        this.Models = Models;
->>>>>>> f1cea58823b15888abb235e7ae546a1955bbb2f7
         this.alterEgo = alterEgo;
     }
     return Car;
